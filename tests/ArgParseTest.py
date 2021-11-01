8b74aab706c9bd34efdcc78d0a0c3347ab0d97f7
Florian Bruggisser <florian@bildspur.ch>
2021-11-01 20:07:11 +0100

    add default vars for input steps

--- tests/ArgParseTest.py
+++ tests/ArgParseTest.py
@@ -1,10 +1,11 @@
 import argparse
 
-from visiongraph.input import InputProviders
+from visiongraph.input import InputProviders, add_input_step_choices
 from visiongraph.util.ArgUtils import add_step_choice_argument
 
 parser = argparse.ArgumentParser("visiongraph", description="just an example help text")
-add_step_choice_argument(parser, InputProviders, "--input", help="Image input provider")
+# add_step_choice_argument(parser, InputProviders, "--input", help="Image input provider")
+add_input_step_choices(parser)
 
 args = parser.parse_args()
 
--- visiongraph/input/__init__.py
+++ visiongraph/input/__init__.py
@@ -1,7 +1,15 @@
+import argparse
+
 from visiongraph.input.RealSenseInput import RealSenseInput
 from visiongraph.input.VideoCaptureInput import VideoCaptureInput
+from visiongraph.util.ArgUtils import add_step_choice_argument
 
 InputProviders = {
     "video-capture": VideoCaptureInput,
     "realsense": RealSenseInput
 }
+
+
+def add_input_step_choices(parser: argparse.ArgumentParser, default: int = 0, add_params: bool = True):
+    add_step_choice_argument(parser, InputProviders, "--input", help="Image input provider",
+                             default=default, add_params=add_params)
