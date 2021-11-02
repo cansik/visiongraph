e573283752b360bd06fb48187e49e34d3e33ed3e
Florian Bruggisser <florian@bildspur.ch>
2021-11-02 12:29:38 +0100

    implemented async frame set recorder

--- visiongraph/recorder/FrameSetRecorder.py
+++ visiongraph/recorder/FrameSetRecorder.py
@@ -1,4 +1,5 @@
 import os
+from queue import Queue
 
 import cv2
 import numpy as np
@@ -9,16 +10,22 @@ from visiongraph.recorder.BaseFrameRecorder import BaseFrameRecorder
 class FrameSetRecorder(BaseFrameRecorder):
     def __init__(self, output_path: str = "recordings"):
         self.output_path = output_path
-        self._frames = []
+        self._frames = Queue()
 
     def open(self):
-        self._frames = []
+        self._frames = Queue()
         os.makedirs(self.output_path, exist_ok=True)
 
     def add_image(self, image: np.ndarray):
-        self._frames.append(image)
+        self._frames.put(image)
 
     def close(self):
-        for i, image in enumerate(self._frames):
-            output_path = os.path.join(self.output_path, f"{i:04d}.png")
-            cv2.imwrite(output_path, image)
+        i = 0
+        while not self._frames.empty():
+            image = self._frames.get()
+            self._write_image(i, image)
+            i += 1
+
+    def _write_image(self, id: int, image: np.ndarray):
+        output_path = os.path.join(self.output_path, f"{id:06d}.png")
+        cv2.imwrite(output_path, image)
