c74ec1db935ec57e5b8cf858e4dbe719aea5745b
Florian Bruggisser <florian@bildspur.ch>
2021-12-02 17:26:36 +0100

    implemented basic openvino yolo inferencing

--- tests/ObjectDetectionExample.py
+++ tests/ObjectDetectionExample.py
@@ -15,7 +15,7 @@ class ObjectDetectionExample(Pipeline):
     def __init__(self, input: BaseInput, multi_threaded: bool = True, deamon: bool = True):
         super().__init__(multi_threaded, deamon)
         self.input = input
-        self.object_detector = YOLODetector.create(YOLOConfig.YOLOv4_Tiny_VOC_416x416_FP32)
+        self.object_detector = YOLODetector.create()
 
         self.add_nodes(self.input, self.object_detector)
 
--- visiongraph/estimator/spatial/YOLODetector.py
+++ visiongraph/estimator/spatial/YOLODetector.py
@@ -1,46 +1,69 @@
 from enum import Enum
-from typing import List
+from typing import List, Optional
 
 import numpy as np
+from openvino.inference_engine import IECore
 
 from visiongraph.data.Asset import Asset
 from visiongraph.data.RepositoryAsset import RepositoryAsset
 from visiongraph.data.labels.COCO import COCO_80_LABELS
-from visiongraph.estimator.openvino.VisionInferenceEngine import VisionInferenceEngine
+from visiongraph.estimator.openvino.SyncInferencePipeline import SyncInferencePipeline
 from visiongraph.estimator.spatial.ObjectDetector import ObjectDetector
+from visiongraph.external.intel.utils import Detection
+from visiongraph.external.intel.yolo import YOLO, YoloV4
+from visiongraph.model.geometry.BoundingBox2D import BoundingBox2D
 from visiongraph.result.spatial.ObjectDetectionResult import ObjectDetectionResult
 
 
+class YOLOArchitecture(Enum):
+    YOLO = 0x1
+    YOLOv4 = 0x2
+
+
 class YOLOConfig(Enum):
-    YOLOv4_Tiny_VOC_256x256_FP32 = (*RepositoryAsset.openVino("yolov4_tiny_voc_256x256_FP32"), COCO_80_LABELS, 256)
-    YOLOv4_Tiny_VOC_416x416_FP32 = (*RepositoryAsset.openVino("yolov4_tiny_voc_416x416_FP32"), COCO_80_LABELS, 416)
+    YOLOv3_FP32 = (*RepositoryAsset.openVino("yolo-v3-tf-fp32"), COCO_80_LABELS, YOLOArchitecture.YOLO)
+    YOLOv3_Tiny_FP32 = (*RepositoryAsset.openVino("yolo-v3-tiny-tf-fp32"), COCO_80_LABELS, YOLOArchitecture.YOLO)
+    YOLOv4_FP32 = (*RepositoryAsset.openVino("yolo-v4-tf-fp32"), COCO_80_LABELS, YOLOArchitecture.YOLOv4)
+    YOLOv4_Tiny_FP32 = (*RepositoryAsset.openVino("yolo-v4-tiny-tf-fp32"), COCO_80_LABELS, YOLOArchitecture.YOLOv4)
 
 
 class YOLODetector(ObjectDetector):
-
     def __init__(self, model: Asset, weights: Asset, labels: List[str],
-                 width: int, height: int, min_score: float = 0.5, nms_threshold: float = 0.5, device: str = "CPU"):
+                 min_score: float = 0.5, nms_threshold: float = 0.5,
+                 architecture: YOLOArchitecture = YOLOArchitecture.YOLOv4, device: str = "CPU"):
         super().__init__(min_score)
+        self.model = model
+        self.weights = weights
         self.labels = labels
+        self.device = device
         self.nms_threshold = nms_threshold
+        self.architecture = architecture
 
-        self.engine = VisionInferenceEngine(model, weights, 1, 3, width, height,
-                                            flip_channels=True, normalize=True, device=device)
+        self.ie = IECore()
+        self.pipeline: Optional[SyncInferencePipeline] = None
+        self.ie_model: Optional[YOLO] = None
 
     def setup(self):
-        self.engine.setup()
+        model_class = YoloV4 if self.architecture == YOLOArchitecture.YOLOv4 else YOLO
+        # download models
+        weights = self.weights.path
+        self.ie_model = model_class(self.ie, self.model.path, self.labels,
+                                    threshold=self.min_score, iou_threshold=self.nms_threshold)
+        self.pipeline = SyncInferencePipeline(self.ie_model, self.device, self.ie)
+        self.pipeline.setup()
 
     def estimate(self, image: np.ndarray, **kwargs) -> List[ObjectDetectionResult]:
-        output = self.engine.estimate(image)
-
-        raise NotImplementedError("YOLO has to be implemented first.")
-        # todo: implement yolo by using the OpenVino Example
-        print(output)
+        h, w = image.shape[:2]
+        output: List[Detection] = self.pipeline.estimate(image)
+        return [ObjectDetectionResult(d.id, self.labels[d.id], d.score,
+                                      BoundingBox2D(d.xmin / w, d.ymin / h,
+                                                    (d.xmax - d.xmin) / w, (d.ymax - d.ymin) / h))
+                for d in output]
 
     def release(self):
-        self.engine.release()
+        self.pipeline.release()
 
     @staticmethod
-    def create(config: YOLOConfig = YOLOConfig.YOLOv4_Tiny_VOC_416x416_FP32) -> "YOLODetector":
-        model, weights, labels, size = config.value
-        return YOLODetector(model, weights, labels, size, size)
+    def create(config: YOLOConfig = YOLOConfig.YOLOv4_Tiny_FP32) -> "YOLODetector":
+        model, weights, labels, architecture = config.value
+        return YOLODetector(model, weights, labels, architecture=architecture)
