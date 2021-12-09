55b36c005b51607fe4a7adf587db7e258da4c4a6
Florian Bruggisser <florian@bildspur.ch>
2021-12-09 15:21:57 +0100

    added configargparse as dependency and pose landmarks

--- visiongraph/result/spatial/pose/PoseLandmarkResult.py
+++ visiongraph/result/spatial/pose/PoseLandmarkResult.py
@@ -1,8 +1,27 @@
 from abc import ABC, abstractmethod
+from typing import List
 
 import vector
 
 from visiongraph.result.spatial.LandmarkDetectionResult import LandmarkDetectionResult
+
+DEFAULT_POSE_LANDMARKS = [
+    "nose",
+    "left_eye",
+    "right_eye",
+    "left_shoulder",
+    "right_shoulder",
+    "left_elbow",
+    "right_elbow",
+    "left_wrist",
+    "right_wrist",
+    "left_hip",
+    "right_hip",
+    "left_knee",
+    "right_knee",
+    "left_ankle",
+    "right_ankle"
+]
 
 
 class PoseLandmarkResult(LandmarkDetectionResult, ABC):
@@ -10,6 +29,10 @@ class PoseLandmarkResult(LandmarkDetectionResult, ABC):
         super().__init__(0, "pose", score, landmarks)
 
     # todo: implement pose connections for base pose landmark
+
+    @property
+    def default_landmarks(self) -> List[vector.Vector4D]:
+        return [getattr(self, lm_name) for lm_name in DEFAULT_POSE_LANDMARKS]
 
     @property
     @abstractmethod
