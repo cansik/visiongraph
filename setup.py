deb2c18749a8f4659f73e557dea5373196ba808b
Florian Bruggisser <florian@bildspur.ch>
2022-01-03 17:39:23 +0100

    upgraded dependencies

--- setup.py
+++ setup.py
@@ -74,7 +74,7 @@ long_description = (current_dir / "README.md").read_text()
 
 setup(
     name="visiongraph",
-    version='0.1.9',
+    version='0.1.10',
     packages=required_packages,
     url='https://github.com/cansik/visiongraph',
     license='MIT License',
