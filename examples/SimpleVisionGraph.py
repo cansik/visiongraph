4750df4bfb03486c3c7b91e2c17ee95a9a335d33
Florian Bruggisser <florian@bildspur.ch>
2022-01-05 00:59:46 +0100

    implemented 3 line graph

--- examples/SimpleVisionGraph.py
+++ examples/SimpleVisionGraph.py
@@ -1,8 +1,5 @@
 # simplest vision graph example
 import visiongraph as vg
 
-graph = vg.VisionGraph({
-    "ssd": vg.ChainEstimator(vg.SSDDetector.create(), vg.ObjectDetectionTracker())
-}, name="Object Detection", input=vg.VideoCaptureInput(), multi_threaded=False)
+graph = vg.VisionGraph({"ssd": vg.ChainEstimator(vg.SSDDetector.create(), vg.ObjectDetectionTracker())}, name="Object Detection", input=vg.VideoCaptureInput(), multi_threaded=False)
 graph.open()
-graph.close()
