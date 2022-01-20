85ff22222bf309669e8c53b63e521821a1743f76
Florian Bruggisser <florian@bildspur.ch>
2022-01-20 14:57:45 +0100

    implemented performance mode

--- examples/PoseEstimationExample.py
+++ examples/PoseEstimationExample.py
@@ -33,12 +33,16 @@ class PoseEstimationExample(BaseGraph):
             result.annotate(frame)
 
         self.fps_tracer.update()
-        cv2.putText(frame, "FPS: %.0f" % self.fps_tracer.smooth_fps,
-                    (7, 40), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 0, 0), 2, cv2.LINE_AA)
+        if not args.performance:
+            cv2.putText(frame, "FPS: %.0f" % self.fps_tracer.smooth_fps,
+                        (7, 40), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 0, 0), 2, cv2.LINE_AA)
 
-        cv2.imshow("Pose Estimator", frame)
-        if cv2.waitKey(15) & 0xFF == 27:
-            self.close()
+            cv2.imshow("Pose Estimator", frame)
+            if cv2.waitKey(15) & 0xFF == 27:
+                self.close()
+        else:
+            print("\033[K", end='')
+            print("FPS: %.0f" % self.fps_tracer.smooth_fps)
 
     @staticmethod
     def add_params(parser: ArgumentParser):
@@ -46,7 +50,7 @@ class PoseEstimationExample(BaseGraph):
 
 
 def main():
-    pipeline = PoseEstimationExample(args.input(),  args.pose_estimator())
+    pipeline = PoseEstimationExample(args.input(), args.pose_estimator())
     pipeline.configure(args)
     pipeline.open()
 
@@ -54,6 +58,7 @@ def main():
 if __name__ == "__main__":
     parser = argparse.ArgumentParser("Pose Estimation Example", description="Example Pipeline")
     add_logging_parameter(parser)
+    parser.add_argument("-p", "--performance", action="store_true", help="Enable performance test (no UI).")
 
     input_group = parser.add_argument_group("input provider")
     add_input_step_choices(input_group)
