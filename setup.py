d3e07d23624b8035ca4b812e7e24966d2e634baf
Florian Bruggisser <florian@bildspur.ch>
2022-01-26 20:41:31 +0100

    prepared new version

--- setup.py
+++ setup.py
@@ -238,7 +238,7 @@ long_description = (current_dir / "README.md").read_text()
 
 setup(
     name="visiongraph",
-    version='0.1.14',
+    version='0.1.15',
     packages=required_packages,
     url='https://github.com/cansik/visiongraph',
     license='MIT License',
