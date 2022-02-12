a6c2876cb624877819ed7b0aa132a6723d423c6f
Florian Bruggisser <florian@bildspur.ch>
2022-02-12 20:14:37 +0100

    enabled more tracking options

--- visiongraph/tracker/MotpyTracker.py
+++ visiongraph/tracker/MotpyTracker.py
@@ -3,20 +3,34 @@ from typing import Optional, List
 
 from visiongraph.GraphNode import GraphNode
 from visiongraph.external.motpy import MultiObjectTracker, Detection
+from visiongraph.external.motpy.tracker import EPS
 from visiongraph.result.ResultList import ResultList
 from visiongraph.result.spatial.ObjectDetectionResult import ObjectDetectionResult
 
 
 class MotpyTracker(GraphNode[ResultList[ObjectDetectionResult], ResultList[ObjectDetectionResult]]):
-    def __init__(self, delta_time: float = 1.0 / 10.0, min_steps_alive: int = 0):
+    def __init__(self, delta_time: float = 1.0 / 10.0,
+                 min_iou: float = 0.1,
+                 multi_match_min_iou: float = 1. + EPS,
+                 min_steps_alive: int = -1,
+                 max_staleness_to_positive_ratio: float = 3.0,
+                 max_staleness: int = 999):
+
         self.delta_time = delta_time
         self.min_steps_alive = min_steps_alive
+        self.min_iou = min_iou
+        self.multi_match_min_iou = multi_match_min_iou
+        self.max_staleness_to_positive_ratio = max_staleness_to_positive_ratio
+        self.max_staleness = max_staleness
 
         self.tracker: Optional[MultiObjectTracker] = None
 
     def setup(self):
         if self.tracker is None:
-            self.tracker = MultiObjectTracker(dt=self.delta_time)
+            self.tracker = MultiObjectTracker(dt=self.delta_time,
+                                              tracker_kwargs={'max_staleness': self.max_staleness},
+                                              matching_fn_kwargs={'min_iou': self.min_iou,
+                                                                  'multi_match_min_iou': self.multi_match_min_iou})
 
     def process(self, data: List[ObjectDetectionResult]) -> ResultList[ObjectDetectionResult]:
         detections = [Detection(box=d.bounding_box.to_array(tl_br_format=True), reference=d)
