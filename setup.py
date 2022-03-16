fa78111345e9e7972e8082054f91105323768883
Florian Bruggisser <florian@bildspur.ch>
2022-03-16 15:54:30 +0100

    fixed vector version

--- setup.py
+++ setup.py
@@ -248,7 +248,7 @@ long_description = (current_dir / "README.md").read_text()
 
 setup(
     name="visiongraph",
-    version='0.1.18',
+    version='0.1.19',
     packages=required_packages,
     url='https://github.com/cansik/visiongraph',
     license='MIT License',
