5e19e0318a3c7306bc39735d316c4e46eeb7a5b5
Florian Bruggisser <florian@bildspur.ch>
2022-03-22 12:19:54 +0100

    added person detection networks based on ssd backend

--- visiongraph/estimator/spatial/SSDDetector.py
+++ visiongraph/estimator/spatial/SSDDetector.py
@@ -9,9 +9,31 @@ from visiongraph.external.intel.model import Model
 from visiongraph.external.intel.ssd import SSD
 from visiongraph.external.intel.utils import InputTransform
 
+_PERSON_LABELS = ["person"]
+
+
+def _person_net(name: str):
+    return *RepositoryAsset.openVino(name), _PERSON_LABELS
+
 
 class SSDConfig(Enum):
     SSDLiteMobileNetV2_FP32 = (*RepositoryAsset.openVino("ssdlite_mobilenet_v2_fp32"), COCO_90_LABELS)
+
+    PersonDetection_0200_256x256_FP16_INT8 = _person_net("person-detection-0200-fp16-int8")
+    PersonDetection_0200_256x256_FP16 = _person_net("person-detection-0200-fp16")
+    PersonDetection_0200_256x256_FP32 = _person_net("person-detection-0200-fp32")
+
+    PersonDetection_0201_384x384_FP16_INT8 = _person_net("person-detection-0201-fp16-int8")
+    PersonDetection_0201_384x384_FP16 = _person_net("person-detection-0201-fp16")
+    PersonDetection_0201_384x384_FP32 = _person_net("person-detection-0201-fp32")
+
+    PersonDetection_0202_512x512_FP16_INT8 = _person_net("person-detection-0202-fp16-int8")
+    PersonDetection_0202_512x512_FP16 = _person_net("person-detection-0202-fp16")
+    PersonDetection_0202_512x512_FP32 = _person_net("person-detection-0202-fp32")
+
+    PersonDetection_0203_864x480_FP16_INT8 = _person_net("person-detection-0203-fp16-int8")
+    PersonDetection_0203_864x480_FP16 = _person_net("person-detection-0203-fp16")
+    PersonDetection_0203_864x480_FP32 = _person_net("person-detection-0203-fp32")
 
 
 class SSDDetector(OpenVinoObjectDetector):
@@ -37,5 +59,3 @@ class SSDDetector(OpenVinoObjectDetector):
 
     def _get_label(self, index: int):
         return super()._get_label(index - 1)
-
-
