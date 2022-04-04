e605c36c277f70320c06e7b700da591821db0076
Florian Bruggisser <florian@bildspur.ch>
2022-04-04 13:31:15 +0200

    added enumeration

--- visiongraph/util/OpenVinoUtils.py
+++ visiongraph/util/OpenVinoUtils.py
@@ -9,14 +9,14 @@ def get_inference_engine_device() -> str:
     devices = ie.available_devices
 
     # try to find preferred GPU device
-    for i, device in devices:
+    for i, device in enumerate(devices):
         if device.startswith("GPU"):
             device_id = i
             break
 
     # list all devices
     logging.info(f"OpenVino Devices")
-    for i, device in devices:
+    for i, device in enumerate(devices):
         default_sign = ""
 
         if i == device_id:
