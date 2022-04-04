9d9d0f33d35300bbb188dc0b677964d066caf0c9
Florian Bruggisser <florian@bildspur.ch>
2022-04-04 13:27:15 +0200

    fixed ie device selection

--- visiongraph/util/OpenVinoUtils.py
+++ visiongraph/util/OpenVinoUtils.py
@@ -6,16 +6,17 @@ def get_inference_engine_device() -> str:
     ie = IECore()
 
     device_id = 0
+    devices = ie.available_devices
 
     # try to find preferred GPU device
-    for i, device in enumerate(ie.available_devices):
+    for i, device in devices:
         if device.startswith("GPU"):
             device_id = i
             break
 
     # list all devices
     logging.info(f"OpenVino Devices")
-    for i, device in enumerate(ie.available_devices):
+    for i, device in devices:
         default_sign = ""
 
         if i == device_id:
@@ -24,4 +25,4 @@ def get_inference_engine_device() -> str:
         full_device_name = ie.get_metric(device, "FULL_DEVICE_NAME")
         logging.info(f"{device}{default_sign}: {full_device_name}")
 
-    return device[device_id]
+    return devices[device_id]
