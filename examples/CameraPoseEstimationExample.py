d785b898eb3e139ce076a5514e9b86a32192dfc1
Florian Bruggisser <github@broox.ch>
2022-04-25 01:26:20 +0200

    implemented camera pose estimation

--- examples/CameraPoseEstimationExample.py
+++ examples/CameraPoseEstimationExample.py
@@ -2,6 +2,7 @@ import argparse
 from argparse import ArgumentParser
 
 import cv2
+import numpy as np
 
 from visiongraph.BaseGraph import BaseGraph
 from visiongraph.estimator.spatial.camera.ArUcoCameraPoseEstimator import ArUcoCameraPoseEstimator
@@ -16,7 +17,13 @@ class CameraPoseEstimationExample(BaseGraph):
         super().__init__()
         self.input = input
 
-        self.network = ArUcoCameraPoseEstimator(None)
+        self.network = ArUcoCameraPoseEstimator(
+            camera_matrix=np.array([[356.92661953, 0., 221.39625447],
+                                    [0., 549.91986118, 377.42600875],
+                                    [0., 0., 1.]], dtype=float),
+            fisheye_distortion=np.array([5.50597053e+00, -1.92784557e+01, 6.93713729e-01,
+                                         1.99492438e-02, 3.38608778e+01], dtype=float)
+        )
 
         self.add_nodes(self.input, self.network)
 
@@ -29,6 +36,9 @@ class CameraPoseEstimationExample(BaseGraph):
         gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
         result = self.network.process(gray)
         result.annotate(frame)
+
+        cv2.putText(frame, f"Distance: {result.position.mag:.2f}",
+                    (5, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
 
         cv2.imshow("Camera Pose Example", frame)
         if cv2.waitKey(15) & 0xFF == 27:
--- visiongraph/estimator/spatial/camera/ArUcoCameraPoseEstimator.py
+++ visiongraph/estimator/spatial/camera/ArUcoCameraPoseEstimator.py
@@ -4,23 +4,25 @@ from typing import Optional, Any
 import cv2
 import numpy as np
 import vector
-from vector import Vector2D, Vector3D
 
 from visiongraph.estimator.VisionEstimator import VisionEstimator
-from visiongraph.input.BaseDepthCamera import BaseDepthCamera
 from visiongraph.result.ArUcoCameraPose import ArUcoCameraPose
 from visiongraph.result.ArUcoMarkerDetection import ArUcoMarkerDetection
 
 
 class ArUcoCameraPoseEstimator(VisionEstimator[Optional[ArUcoCameraPose]]):
-    def __init__(self, camera: BaseDepthCamera,
+    def __init__(self,
+                 camera_matrix: np.ndarray,
+                 fisheye_distortion: np.ndarray,
                  aruco_config: int = cv2.aruco.DICT_6X6_50,
-                 marker_size_in_m: float = 1.0,
+                 marker_length_in_m: float = 0.1,
                  marker_height_in_m: float = 1.5):
-        self.camera = camera
+        self.camera_matrix = camera_matrix
+        self.fisheye_distortion = fisheye_distortion
+
         self.aruco_config: int = aruco_config
 
-        self.marker_size_in_m: float = marker_size_in_m
+        self.marker_size_in_m: float = marker_length_in_m
         self.marker_height_in_m: float = marker_height_in_m
 
         self.aruco_dict: Optional[Any] = None
@@ -51,7 +53,21 @@ class ArUcoCameraPoseEstimator(VisionEstimator[Optional[ArUcoCameraPose]]):
                                       vector.obj(x=bottomRight[0], y=bottomRight[1]),
                                       vector.obj(x=bottomLeft[0], y=bottomLeft[1]))
 
-        return ArUcoCameraPose(Vector3D(), Vector3D(), marker)
+        # estimate pose
+        rotation_vector, translation_vector, _ = cv2.aruco.estimatePoseSingleMarkers([marker_corner], 0.05,
+                                                                                     self.camera_matrix,
+                                                                                     self.fisheye_distortion)
+
+        cv2.drawFrameAxes(data, self.camera_matrix, self.fisheye_distortion, rotation_vector, translation_vector, 0.1)
+
+        # todo: maybe flip the position parameter
+        return ArUcoCameraPose(position=vector.obj(x=translation_vector[0, 0, 0],
+                                                   y=translation_vector[0, 0, 1],
+                                                   z=translation_vector[0, 0, 2]),
+                               rotation=vector.obj(x=rotation_vector[0, 0, 0],
+                                                   y=rotation_vector[0, 0, 1],
+                                                   z=rotation_vector[0, 0, 2]),
+                               marker=marker)
 
     def release(self):
         pass
