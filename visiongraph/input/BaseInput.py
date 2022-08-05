af9ed8df6c12b7c091f9eeacd8117791d371e494
Florian Bruggisser <github@broox.ch>
2022-08-05 11:12:42 +0200

    changed fps parameter to float value

--- visiongraph/input/BaseInput.py
+++ visiongraph/input/BaseInput.py
@@ -15,9 +15,9 @@ from visiongraph.util.ArgUtils import add_dict_choice_argument
 class BaseInput(GraphNode[None, np.ndarray], ABC):
     @abstractmethod
     def __init__(self):
-        self.width = 640
-        self.height = 480
-        self.fps = 30
+        self.width: int = 640
+        self.height: int = 480
+        self.fps: float = 30.0
 
         self.rotate: Optional[int] = None
         self.flip: Optional[int] = None
@@ -62,7 +62,7 @@ class BaseInput(GraphNode[None, np.ndarray], ABC):
     @abstractmethod
     def configure(self, args: Namespace):
         self.width, self.height = args.input_size
-        self.fps = args.input_fps
+        self.fps = float(args.input_fps)
         self.rotate = args.input_rotate
         self.flip = args.input_flip
         self.raw_input = args.raw_input
@@ -82,7 +82,7 @@ class BaseInput(GraphNode[None, np.ndarray], ABC):
         try:
             parser.add_argument("--input-size", default=[640, 480], type=int, nargs=2, metavar=("width", "height"),
                                 help="Requested input media size.")
-            parser.add_argument("--input-fps", default=30, type=int, help="Requested input media framerate.")
+            parser.add_argument("--input-fps", default=30, type=float, help="Requested input media framerate.")
             add_dict_choice_argument(parser, RotationParameter, "--input-rotate", help="Rotate input media",
                                      default=None)
             add_dict_choice_argument(parser, FlipParameter, "--input-flip", help="Flip input media", default=None)
