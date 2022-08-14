b3c42280bdff95976c5edd4716c7c9b2649fcde2
Florian Bruggisser <florian@bildspur.ch>
2022-08-14 12:56:49 +0200

    updated auto-exposure test

--- tests/RealSenseAutoExposureLimit.py
+++ tests/RealSenseAutoExposureLimit.py
@@ -9,4 +9,4 @@ depth_sensor: rs.depth_sensor = device.first_depth_sensor()
 depth_sensor.set_option(rs.option.auto_exposure_limit, 500.0)
 
 # does not work and results in a wrong error message
-depth_sensor.set_option(rs.option.auto_exposure_limit, 0.0)
+depth_sensor.set_option(rs.option.auto_exposure_limit, 0)
