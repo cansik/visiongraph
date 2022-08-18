f40b49aefc0aff76da87937ce3e40999a2dd5f95
Florian Bruggisser <github@broox.ch>
2022-08-18 22:17:26 +0200

    removed charuco initial guess

--- visiongraph/estimator/spatial/camera/ChArUcoCalibrator.py
+++ visiongraph/estimator/spatial/camera/ChArUcoCalibrator.py
@@ -60,18 +60,11 @@ class ChArUcoCalibrator(BoardCameraCalibrator):
         if len(corners) > 0:
             self.image_size = gray.shape[::-1]
 
-            for corner in corners:
-                # todo: check if this does a refinement at all
-                cv2.cornerSubPix(gray, corner,
-                                 winSize=(3, 3),
-                                 zeroZone=(-1, -1),
-                                 criteria=self.criteria)
+            ret, charuco_corners, charuco_ids = aruco.interpolateCornersCharuco(corners, ids, gray, self.board)
 
-            res2 = aruco.interpolateCornersCharuco(corners, ids, gray, self.board)
-
-            if res2[1] is not None and res2[2] is not None and len(res2[1]) > 3:
-                self.corners.append(res2[1])
-                self.ids.append(res2[2])
+            if ret and len(charuco_corners) > 3:
+                self.corners.append(charuco_corners)
+                self.ids.append(charuco_ids)
 
                 aruco.drawDetectedMarkers(data, corners, ids)
                 self.board_detected = True
@@ -82,12 +75,6 @@ class ChArUcoCalibrator(BoardCameraCalibrator):
         return None
 
     def calibrate(self) -> Optional[CameraPoseResult]:
-        camera_mat_init = np.array([[1000., 0., self.image_size[0] / 2.],
-                                    [0., 1000., self.image_size[1] / 2.],
-                                    [0., 0., 1.]])
-
-        dist_coeffs_init = np.zeros((5, 1))
-        flags = (cv2.CALIB_USE_INTRINSIC_GUESS + cv2.CALIB_RATIONAL_MODEL + cv2.CALIB_FIX_ASPECT_RATIO)
         (ret, camera_matrix, distortion_coefficients,
          rotation_vectors, translation_vectors,
          std_deviations_intrinsics, std_deviations_extrinsics,
@@ -96,13 +83,12 @@ class ChArUcoCalibrator(BoardCameraCalibrator):
             charucoIds=self.ids,
             board=self.board,
             imageSize=self.image_size,
-            cameraMatrix=camera_mat_init,
-            distCoeffs=dist_coeffs_init,
-            flags=flags,
-            criteria=(cv2.TERM_CRITERIA_EPS & cv2.TERM_CRITERIA_COUNT, 10000, 1e-9))
+            cameraMatrix=None,
+            distCoeffs=None,
+            criteria=self.criteria)
 
         if ret:
-            intrinsics = CameraIntrinsics(camera_matrix, distortion_coefficients)
+            intrinsics = CameraIntrinsics(camera_matrix, distortion_coefficients.flatten())
             self.pose_result = CameraPoseResult(intrinsics)
             return self.pose_result
 
