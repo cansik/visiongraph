2b7751d296ba6f6fd1988a7bbdc75712bf137e7a
Florian Bruggisser <florian@bildspur.ch>
2022-11-28 10:26:07 +0100

    implemented easy accessors for azure kinect

--- setup.py
+++ setup.py
@@ -256,7 +256,7 @@ long_description = (current_dir / "README.md").read_text()
 
 setup(
     name="visiongraph",
-    version="0.1.31.2",
+    version="0.1.31.3",
     packages=required_packages,
     url="https://github.com/cansik/visiongraph",
     license="MIT License",
