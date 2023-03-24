6e16cd413fb40105a32ecaf08866454cc7eb0114
Florian Bruggisser <github@broox.ch>
2023-03-24 17:10:57 +0100

    changed source short argument to src

--- visiongraph/util/CommonArgs.py
+++ visiongraph/util/CommonArgs.py
@@ -3,7 +3,7 @@ import argparse
 
 def add_source_argument(parser: argparse.ArgumentParser):
     try:
-        parser.add_argument("-s", "--source", type=str, help="Generic input source for all inputs.")
+        parser.add_argument("-src", "--source", type=str, help="Generic input source for all inputs.")
     except argparse.ArgumentError as ex:
         if ex.message.startswith("conflicting"):
             return
