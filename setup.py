f32a5fb66f083cd5323b844e2b4c60cc41ecdd5f
Florian Bruggisser <github@broox.ch>
2023-06-07 00:07:21 +0200

    updated to modern mediapipe and onnx

--- setup.py
+++ setup.py
@@ -267,7 +267,7 @@ long_description = (current_dir / "README.md").read_text()
 
 setup(
     name="visiongraph",
-    version="0.1.45.1",
+    version="0.1.46",
     packages=required_packages,
     url="https://github.com/cansik/visiongraph",
     license="MIT License",
