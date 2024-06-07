8982abfe06617297f59d3f41b1a9c2cc68cc35d5
Florian Bruggisser <github@broox.ch>
2024-06-07 17:44:15 +0200

    bumped version

--- setup.py
+++ setup.py
@@ -270,7 +270,7 @@ long_description = (current_dir / "README.md").read_text()
 
 setup(
     name="visiongraph",
-    version="0.1.58",
+    version="0.1.58.1",
     packages=required_packages,
     url="https://github.com/cansik/visiongraph",
     license="MIT License",
