9306a3d893047ee7193415757b388bd32efa3c05
Florian Bruggisser <github@broox.ch>
2024-09-25 17:35:28 +0200

    adjusted formatting script

--- format-code.sh
+++ format-code.sh
@@ -1,4 +1,11 @@
 #!/bin/zsh
 
-# format all python files
-find ./visiongraph -name '*.py' -exec autopep8 --in-place '{}' --max-line-length 120 \;
+# List of directories to search for Python files
+folders=(./visiongraph ./examples ./scripts ./snippets ./tests ./tools)
+
+# Loop through each folder and format Python files using autopep8
+for folder in "${folders[@]}"
+do
+  echo "formatting $folder..."
+  find "$folder" -name '*.py' -exec autopep8 --in-place '{}' --max-line-length 120 \;
+done
