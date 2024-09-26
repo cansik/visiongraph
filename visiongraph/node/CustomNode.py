27152af85f3ec2032974aad59e8fe88689e9e719
Florian Bruggisser <github@broox.ch>
2024-09-26 15:59:39 +0200

    documented nodes

--- visiongraph/node/CustomNode.py
+++ visiongraph/node/CustomNode.py
@@ -8,15 +8,42 @@ OutputType = TypeVar('OutputType')
 
 
 class CustomNode(GraphNode[InputType, OutputType]):
+    """
+    A custom node for graph-based computations.
+    """
+
     def __init__(self, method: Callable[[InputType], OutputType], *args, **kwargs):
+        """
+        Initializes a new instance of CustomNode.
+
+        Args:
+            method (Callable[[InputType], OutputType]): The function to be executed on the input data.
+            *args: Additional positional arguments passed to the `method`.
+            **kwargs: Additional keyword arguments passed to the `method`.
+
+        Returns:
+            None
+        """
         self.method = method
         self.args = args
         self.kwargs = kwargs
 
     def setup(self):
+        """
+        Setup the node for execution.
+        """
         pass
 
     def process(self, data: InputType) -> OutputType:
+        """
+        Executes the `method` on the input data.
+
+        Args:
+            data (InputType): The input data to be processed.
+
+        Returns:
+            OutputType: The result of executing the `method`.
+        """
         result = self.method(data, *self.args, **self.kwargs)
 
         if result is None:
@@ -25,11 +52,26 @@ class CustomNode(GraphNode[InputType, OutputType]):
         return result
 
     def release(self):
+        """
+        Releases any resources held by the node.
+        """
         pass
 
     def configure(self, args: Namespace):
+        """
+        Configures the node based on the provided arguments.
+
+        Args:
+            args (Namespace): The parsed command-line arguments.
+        """
         pass
 
     @staticmethod
     def add_params(parser: ArgumentParser):
+        """
+        Adds parameters to the parser for this custom node.
+
+        Args:
+            parser (ArgumentParser): The parser to add parameters to.
+        """
         pass
--- visiongraph/node/SequenceNode.py
+++ visiongraph/node/SequenceNode.py
@@ -8,27 +8,64 @@ OutputType = TypeVar('OutputType')
 
 
 class SequenceNode(GraphNode[InputType, OutputType]):
+    """
+    Represents a node in a sequence of nodes.
+    """
+
     def __init__(self, *nodes: GraphNode):
+        """
+        Initializes the SequenceNode with a list of child nodes.
+
+        Args:
+            *nodes (GraphNode): The child nodes to be initialized.
+        """
         self.nodes = nodes
 
-    def setup(self):
+    def setup(self) -> None:
+        """
+        Sets up all the child nodes in the sequence.
+        """
         for node in self.nodes:
             node.setup()
 
     def process(self, data: InputType) -> OutputType:
+        """
+        Processes the input data through each node in the sequence.
+
+        Args:
+            data (InputType): The input data to be processed.
+
+        Returns:
+            OutputType: The output data after processing.
+        """
         temp = data
         for node in self.nodes:
             temp = node.process(temp)
         return temp
 
-    def release(self):
+    def release(self) -> None:
+        """
+        Releases the resources held by all the child nodes in the sequence.
+        """
         for node in self.nodes:
             node.release()
 
-    def configure(self, args: Namespace):
+    def configure(self, args: Namespace) -> None:
+        """
+        Configures all the child nodes in the sequence based on the provided arguments.
+
+        Args:
+            args (Namespace): The namespace containing the configuration arguments.
+        """
         for node in self.nodes:
             node.configure(args)
 
     @staticmethod
-    def add_params(parser: ArgumentParser):
+    def add_params(parser: ArgumentParser) -> None:
+        """
+        Adds parameters to the parser that are specific to this sequence node.
+
+        Args:
+            parser (ArgumentParser): The parser to be updated.
+        """
         pass
