2ab8294156e3a9e0453401236757a9c090a60938
Florian Bruggisser <github@broox.ch>
2024-12-13 11:58:22 +0100

    bumped version to 1.0.1

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ ALL_NAME = "all"
 
 NAME = "visiongraph"
 PACKAGE_NAME = NAME
-PACKAGE_VERSION = "1.0.0"
+PACKAGE_VERSION = "1.0.1"
 PACKAGE_URL = "https://github.com/cansik/visiongraph"
 PACKAGE_DOC_MODULES = ["visiongraph", "!visiongraph.external"]
 
